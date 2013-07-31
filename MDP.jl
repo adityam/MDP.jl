26bae6f0abd4fe261ea2052925bab0f8b431253b
Aditya Mahajan <adityam@umich.edu>
2013-07-31 19:00:24 -0400

    Add a withIndex method
    Julia does not have an inbuilt function similar to MATLAB's max/min which
    returns the max as well as the arg max. The function withIndex is a replacement
    for that.
    
    I still need to look to see if this function can be made more efficient.

--- MDP.jl
+++ MDP.jl
@@ -10,6 +10,7 @@ module MDP
         perStep    :: Matrix{Float64}
         transition :: Matrix{Float64}
         objective  :: Function
+        findIndex  :: Function
 
         function Model(c,P;objective=:Max) 
             (n,m) = size(c)
@@ -18,8 +19,8 @@ module MDP
             elseif objective != :Max && objective != :Min 
                 error("Model sense must be :Max or :Min")
             else
-                obj = (objective == :Max)? max : min
-                new (n,m,c,P,obj)
+                obj,ind = (objective == :Max)? (max,indmax) : (min,indmin)
+                new (n,m,c,P,obj,ind)
             end
         end
     end
@@ -28,9 +29,10 @@ module MDP
         Q = m.perStep + discount *
                reshape(m.transition * valueFunction, m.stateSize, m.actionSize);
 
-        vUpdated = m.objective(Q,(),2)
+        # vUpdated = m.objective(Q,(),2)
+        vUpdated, gOptimal = withIndex(m.findIndex,Q)
 
-        return vec(vUpdated)
+        return vec(vUpdated), vec(gOptimal)
     end
 
     function valueIteration(m::Model;
@@ -44,12 +46,12 @@ module MDP
         
         update(v) = bellmanUpdate(m,v; discount=discount)
 
-        v          = update(initial_v)
+        v,g        = update(initial_v)
         v_previous = copy(v)
 
         for i = 1:iterations
             copy!(v_previous, v)
-            v = update(v_previous)
+            v,g = update(v_previous)
             
             if spanNorm(v, v_previous) < scaledTolerance
                 println("Reached precision ", tolerance, " at iteration ", i)
@@ -60,12 +62,21 @@ module MDP
         # Renormalize v -- See Puterman 6.6.12 for details
         v += m.objective(v - v_previous)/scale
 
-        return v
+        return v,g
     end
 
     function spanNorm (x::Vector, y::Vector)
         z = x - y;
         return max(z) - min(z)
+    end
+
+    # Julia does not have an in-built function that returns the minimum and the
+    # arg min.
+    function withIndex{T}(f::Function, x::Matrix{T})
+        idx = vec(mapslices(f,x,2))
+        val :: Vector{T} = [x[i,idx[i]] for i = 1:size(x,1) ]
+
+        return val,idx
     end
 
 end
