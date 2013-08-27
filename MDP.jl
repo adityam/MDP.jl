7e88207c8a50f4980263d943f298bde5066780ca
Aditya Mahajan <adityam@umich.edu>
2013-08-27 00:05:33 -0400

    Code for centralized example

--- MDP.jl
+++ MDP.jl
@@ -1,18 +1,21 @@
 module MDP
 
-    export ProbModel,
+    export Model,
+           ProbModel,
            DynamicModel,
            bellmanUpdate,
            valueIteration,
            valueIterationBound
 
-    type ProbModel
+
+    abstract Model
+
+    type ProbModel <: Model
+        bellmanUpdate :: Function
+        objective  :: Function
+        contractionFactor :: Float64
         stateSize  :: Int32
         actionSize :: Int32
-        perStep    :: Matrix{Float64}
-        transition :: Matrix{Float64}
-        objective  :: Function
-        compare    :: Function
 
         function ProbModel(c, P; objective=:Max) 
             (n, m) = size(c)
@@ -22,92 +25,48 @@ module MDP
                 error("Model sense must be :Max or :Min")
             else
                 obj, cmp = (objective == :Max)? (max, >) : (min, <)
-                new (n, m, c, P, obj, cmp)
+
+                function bellman(v::Vector{Float64}; discount=1.0)
+                    Q = c + discount * reshape(P * v, n, m);
+
+                    # vUpdated = m.objective(Q, (), 2)
+                    vUpdated, gOptimal = withIndex(cmp, Q)
+
+                    return vec(vUpdated), vec(gOptimal)
+                end
+
+                # See Puterman Thm 6.6.6
+                contractionFactor = 1 - sum(min(P, (), 1))
+                new (bellman, obj, contractionFactor, n, m)
             end
         end
     end
 
-    type DynamicModel
+    type DynamicModel <: Model
         bellmanUpdate :: Function # (valueFunction; discount=1.0) -> (valueFunction, policy)
         objective  :: Function
+        contractionFactor :: Float64
 
-        function DynamicModel(bellmanUpdate; objective=:Max) 
+        function DynamicModel(bellmanUpdate; contractionFactor=1.0, objective=:Max) 
             if objective != :Max && objective != :Min 
                 error("Model sense must be :Max or :Min")
             else
                 obj = (objective == :Max)? max : min
-                new (bellmanUpdate, obj)
+                new (bellmanUpdate, obj, contractionFactor)
             end
         end
-    end
-
-    function bellmanUpdate(m::ProbModel, valueFunction::Vector{Float64}; discount=1.0)
-        Q = m.perStep + discount *
-               reshape(m.transition * valueFunction, m.stateSize, m.actionSize);
-
-        # vUpdated = m.objective(Q, (), 2)
-        vUpdated, gOptimal = withIndex(m.compare, Q)
-
-        return vec(vUpdated), vec(gOptimal)
-    end
-
-    function valueIteration(m::ProbModel;
-                    initial_v  :: Vector{Float64} = vec(zeros(m.stateSize)),
-                    discount   :: Float64 = 0.95,
-                    iterations :: Int32   = 1_000,
-                    tolerance  :: Float64 = 1e-4)
-      
-        scale = (discount < 1)? (1-discount)/discount : 1.0
-        update(v) = bellmanUpdate(m,v; discount=discount)
-
-        return valueUpdateInnerLoop(initial_v, update, m.objective;
-                    iterations = iterations,
-                    scale      = scale,
-                    tolerance  = tolerance)
 
     end
 
-    function valueIteration(m::DynamicModel, initial_v :: Array{Float64};
+    function valueIteration(m::Model, initial_v :: Array{Float64};
                     discount   :: Float64 = 0.95,
                     iterations :: Int32   = 1_000,
                     tolerance  :: Float64 = 1e-4)
 
         scale = (discount < 1)? (1-discount)/discount : 1.0
+        scaledTolerance = scale * tolerance / 2.0
+
         update(v) = m.bellmanUpdate(v; discount=discount)
-
-        return valueUpdateInnerLoop(initial_v, update, m.objective;
-                    iterations = iterations,
-                    scale      = scale,
-                    tolerance  = tolerance)
-                    
-    end
-
-    function valueIterationBound(m::ProbModel;
-                          initial_v  :: Vector{Float64} = vec(zeros(m.stateSize)),
-                          discount   :: Float64 = 0.95,
-                          tolerance  :: Float64 = 1e-4)
-
-        scale = (discount < 1)? (1-discount)/discount : 1.0
-        scaledTolerance = scale * tolerance / 2.0
-
-        # See Puterman Thm 6.6.6
-        k = 1 - sum(min(m.transition, (), 1))
-
-        v,_        = bellmanUpdate(m, initial_v; discount=discount)
-        precision  = spanNorm(v, initial_v)
-
-        # See Puterman Prop 6.6.5
-        iterations = log( scaledTolerance/precision ) / log ( k*discount ) 
-
-        return int(iterations + 1)
-    end
-        
-    function valueUpdateInnerLoop(initial_v, update, objective; 
-                    iterations :: Int32   = 1_000,
-                    scale      :: Float64 = 1e-2,
-                    tolerance  :: Float64 = 1e-4)
-
-        scaledTolerance = scale * tolerance / 2.0
         
         (v, g)     = update(initial_v)
         v_previous = copy(v)
@@ -126,11 +85,53 @@ module MDP
         @printf("Reached precision %e at iteration %d\n", 2.0*precision/scale, iterationCount)
 
         # Renormalize v -- See Puterman 6.6.12 for details
-        v += objective(v - v_previous)/scale
+        v += m.objective(v - v_previous)/scale
 
         return (v, g)
     end
 
+    function valueIterationBound(m::Model, initial_v :: Array{Float64};
+                          discount   :: Float64 = 0.95,
+                          tolerance  :: Float64 = 1e-4)
+        scale = (discount < 1)? (1-discount)/discount : 1.0
+        scaledTolerance = scale * tolerance / 2.0
+
+        v,_        = m.bellmanUpdate(initial_v; discount=discount)
+        precision  = spanNorm(v, initial_v)
+
+        # See Puterman Prop 6.6.5
+        # We compare with zero to allow overflow errors when precision is 0.
+        iterations = abs(precision)<4*eps(Float64)? 1 :
+                     log( scaledTolerance/precision ) / log ( m.contractionFactor*discount ) 
+
+        return int(iterations + 1)
+    end
+
+    # For the probability model, it is possible to automatically initialize 
+    # the initial vector
+    function valueIteration(m::ProbModel;
+                    initial_v  :: Vector{Float64} = vec(zeros(m.stateSize)),
+                    discount   :: Float64 = 0.95,
+                    iterations :: Int32   = 1_000,
+                    tolerance  :: Float64 = 1e-4)
+      
+        return valueIteration(m, initial_v;
+                    discount   = discount,
+                    iterations = iterations,
+                    tolerance  = tolerance)
+    end
+
+
+    function valueIterationBound(m::ProbModel;
+                          initial_v  :: Vector{Float64} = vec(zeros(m.stateSize)),
+                          discount   :: Float64 = 0.95,
+                          tolerance  :: Float64 = 1e-4)
+
+        return valueIterationBound(m, initial_v;
+                    discount   = discount,
+                    tolerance  = tolerance)
+    end
+        
     function spanNorm{T <: Real} (x::Array{T}, y::Array{T})
         z = x - y;
         return max(z) - min(z)
---
+++ channel.jl
@@ -0,0 +1,76 @@
+using MDP
+
+# Only needed for rle function
+using Stats
+
+L = 100
+alpha = [0.75 0.75]
+
+P = [ alpha[1]		1-alpha[1]
+	  1 - alpha[2]  alpha[2]  ]
+
+# Initialize xi (could be optimized)
+xi = [ P^n for n = 1:L+1] 
+
+r = 1.0
+
+for c = [0.1 0.2 0.3 0.4 0.5]
+    for p = [0.1 0.2 0.3 0.4]
+        q = 1 - p
+
+        function bellmanUpdate(v; discount=1.0)
+            # Assume v is 2 x 2 x (L+1)
+            v_next = zero(v)
+            g_next = zeros(size(v))
+
+            # v_next(n, s, l)
+            # n = 1 == 0 packets in buffer
+            # n = 2 == 1 packet in buffer
+
+            # s is the last channel state observed
+            # s = 1 == Idle
+            # s = 2 == Busy
+
+            # ell is the time when the last observation was made
+            for s = 1:2 
+                for ell = 1:L+1
+                    next_ell = min(ell+1, L+1)
+                    v_next[1, s, ell] = discount * (q * v[1, s, next_ell] + p * v[2, s, next_ell])
+                    g_next[1, s, ell] = 1
+
+                    next = [ discount * v[2, s, next_ell] 
+                             xi[ell][s,1]*r - c + discount * ( xi[ell][s,1] * ( q * v[1, 1, 1] + p * v[2, 1, 1])
+                                                             + xi[ell][s,2] * v[2, 2, 1]) ] 
+                    g_next[2, s, ell] = indmax(next)
+                    v_next[2, s, ell] = next[ g_next[2, s, ell] ] 
+                end
+            end
+
+            return (v_next, g_next)
+        end
+
+        model = DynamicModel(bellmanUpdate; objective=:Max)
+        v_initial = zeros(2,2,L+1)
+
+        # @printf("Should take atmost %d iterations\n", valueIterationBound(model, v_initial; discount=0.9))
+
+        (v,g) = valueIteration(model, v_initial; discount=0.9)
+
+        g1 = vec(g[2,1,:])
+        g2 = vec(g[2,2,:])
+
+        a1,b1 = rle(g1[1:end-1])
+        a2,b2 = rle(g2[1:end-1])
+
+        k1 = (a1[1] == 1.0)?(b1[1]+1):1
+        k2 = (a2[1] == 1.0)?(b2[1]+1):1
+
+        @printf("c=%0.2f\tp=%0.2f\t(%d,%d)\n", c,p, k1,k2)
+  end
+end
+
+# v1 = vec(v[2,1,:])
+# v2 = vec(v[2,2,:])
+# 
+# println(v1)
+# println(v2)
