e597bd5665ba934b8cdad85389dad16ea074d2de
Aditya Mahajan <adityam@umich.edu>
2015-01-31 12:17:54 -0500

    Reward = -Inf for disabled actions

--- puterman-3.1.jl
+++ puterman-3.1.jl
@@ -6,10 +6,10 @@ P = { [ 0.5  0.5
         0.5  0.5 ] }
 
 r = [ 5  10
-     -1  -100 ]
+     -1  -Inf ]
 
 model = ProbModel(r, P; objective=:Max)
 
-(v,g) = valueIteration(model; discount=0.95, tolerance=0.01)
+(v,g) = valueIteration(model; discount=0.95)
 
 print(v)
