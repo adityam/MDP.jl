37679ede965709ed70f507e6adb34e727db5c573
Aditya Mahajan <adityam@umich.edu>
2015-02-03 11:55:38 -0500

    Do not use deprecated syntax
    Matrix[...,...] instead of {...,...}

--- test/valueIteration.jl
+++ test/valueIteration.jl
@@ -3,15 +3,15 @@ using Base.Test
 
 # Puterman, Example 3.1
 
-P_01 = { [ 0.5  0.5 
-          0.0  1.0 ],
-        [ 0.0  1.0
-          0.5  0.5 ] }
+P_01 = Matrix[ [ 0.5  0.5 
+                 0.0  1.0 ],
+               [ 0.0  1.0
+                 0.5  0.5 ] ]
 
 r_01 = [ 5  10
         -1  -Inf ]
 
-model_01 = ProbModel(r_01, P_01; objective=:Max)
+model_01 = ProbModel(r_01, P_01; objective = :Max)
 
 discount_01  = 0.95
 tolerance_01 = 1e-3
@@ -22,4 +22,3 @@ v_opt_01 = [ (5.0 - 5.5 * discount_01)/( (1.0 - 0.5 * discount_01) * (1.0 - disc
             -1.0 / ( 1 - discount_01 ) ]
 
 @test_approx_eq_eps v_01 v_opt_01 tolerance_01
-
