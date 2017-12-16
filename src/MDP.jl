33efeda27c029d63d114e9101944c8695ecf878b
Aditya Mahajan <adityam@umich.edu>
2017-12-16 08:49:29 -0500

    Change iteration_bound to Int rather than Float
    Somewhat annoyingly, `floor` returns a `Float` rather than an `Int`.

--- src/MDP.jl
+++ src/MDP.jl
@@ -102,7 +102,7 @@ module MDP
             if abs(v_precision) < 4*eps(Float64)
                 iteration_bound = 1
             else
-                iteration_bound = floor(log( scaledTolerance/v_precision ) / log( m.contractionFactor*discount ))
+                iteration_bound = floor(Int, log( scaledTolerance/v_precision ) / log( m.contractionFactor*discount ))
             end
 
             info("value iteration will converge in at most $iteration_bound iterations")
